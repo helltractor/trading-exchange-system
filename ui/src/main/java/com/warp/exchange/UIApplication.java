05d90615633a813b8a699e64a2412b76e628f015
helltractor <1664309610@qq.com>
2024-06-12 16:28:25 +0800

    Add ui

--- ui/src/main/java/com/warp/exchange/UIApplication.java
+++ ui/src/main/java/com/warp/exchange/UIApplication.java
@@ -1,11 +1,43 @@
 package com.warp.exchange;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.warp.exchange.client.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UIApplication {
     public static void main(String[] args) {
         SpringApplication.run(UIApplication.class, args);
+    }
+    
+    @Bean
+    public RestClient createRestClient(
+            @Value("#{exchangeConfiguration.apiEndpoints.tradingApi}") String tradingApiEndpoint,
+            @Autowired ObjectMapper objectMapper) {
+        return new RestClient.Builder(tradingApiEndpoint).build(objectMapper);
+    }
+    
+    /**
+     * webMvcConfigurer用于自定义Spring MVC的配置。
+     *
+     * @return
+     */
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            /**
+             * Keep "/static/" prefix
+             */
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+            }
+        };
     }
 }

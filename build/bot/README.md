edb9990c00fd5b2a5aa204bcfe84b62040cfadf9
helltractor <1664309610@qq.com>
2024-12-17 13:41:50 +0800

    chore(build): update bot script and docker-compose.yml

--- build/bot/README.md
+++ build/bot/README.md
@@ -3,6 +3,7 @@
 Use `bot.py` to do automatically trading.
 
 command:
+
 ```
 bot.py --email=user0@example.com --password=password0
 ```
--- build/bot/batch.py
+++ build/bot/batch.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 
 py_dir = sys.executable
 script_dir = f"{os.getcwd()}/bot.py"
-
 for i in range(10):
-    command = f'start cmd /k "{py_dir} {script_dir} --email=user{i}@example.com --password=password{i}"'
+    if os.name == 'nt':
+        command = f'start cmd /k "{py_dir} {script_dir} --email=user{i}@example.com --password=password{i}"'
+    else:
+        command = f'xterm -e "{py_dir} {script_dir} --email=user{i}@example.com --password=password{i}; bash" &'
     os.system(command)
